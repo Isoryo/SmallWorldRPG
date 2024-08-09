080d3b884a126ff9c840186546c65c5d8d32fd5b
Isoryo <isoryo0709@gmail.com>
2024-08-09 18:08:32 +0900

    UnityChanの導入と歩行アニメーションの導入

---
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
